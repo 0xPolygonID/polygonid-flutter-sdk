d0e1636015da48e76cdf37b284e828f75942aa8c
Raul Jareño <raul.jareno.diaz@gmail.com>
2021-11-18 13:20:30 +0100

    fixed issues with babyjubjub library

--- lib/utils/structs.dart
+++ lib/utils/structs.dart
@@ -8,7 +8,7 @@ class Signature extends Struct {
 
   external Pointer<Uint8>? s;
 
-  factory Signature.allocate(Pointer<Point>? rB8, Pointer<Uint8>? s) =>
+  static Signature allocate(Pointer<Point>? rB8, Pointer<Uint8>? s) =>
       calloc<Signature>().ref
         ..r_b8 = rB8
         ..s = s;
@@ -21,7 +21,7 @@ class Point extends Struct {
 
   external Pointer<Point>? address;
 
-  factory Point.allocate(Pointer<Uint8>? x, Pointer<Uint8>? y) {
+  static Point allocate(Pointer<Uint8>? x, Pointer<Uint8>? y) {
     final pointer = calloc<Point>();
     return pointer.ref
       ..address = pointer
