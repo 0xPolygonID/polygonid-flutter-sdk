453a809b66e9d0bffb76687c48d38cad286f1225
Raul Jareño <raul.jareno.diaz@gmail.com>
2021-12-06 16:55:44 +0000

    implemented examples of ide3 core library

--- ios/Classes/SwiftPrivadoidPlugin.swift
+++ ios/Classes/SwiftPrivadoidPlugin.swift
@@ -22,18 +22,23 @@ public class SwiftPrivadoidPlugin: NSObject, FlutterPlugin {
   }
     
     public func testNewClaim() -> String {
+        // var mtRoot = nil
+        // var idGenesis = nil
+        
         let schemaHash : [UInt8] = [0x52, 0xFD, 0xFC, 0x07, 0x21, 0x82, 0x65, 0x4F, 0x16, 0x3F, 0x5F, 0x0F, 0x9A, 0x62, 0x1D, 0x72]
         let unsafePointerSchemaHash : UnsafeMutablePointer<UInt8> = UnsafeMutablePointer<UInt8>(mutating: schemaHash)
         
+        // X = 15468939102716291673743744296736132867654217747684906302563904432835075522918
         let XVal : [UInt8] = [0x66, 0x41, 0x8C, 0x4D, 0x7F, 0x25, 0x2C, 0xC2, 0x3E, 0xDF, 0x63, 0xE4, 0x5D, 0xBD, 0x98, 0x45, 0x57, 0xD8, 0xA4, 0xDA, 0x41, 0x2C, 0xC8, 0x12, 0x64, 0x16, 0xE0, 0xA4, 0xF8, 0x1B, 0x33, 0x22]
         let unsafePointerX : UnsafeMutablePointer<UInt8> = UnsafeMutablePointer<UInt8>(mutating: XVal)
-        var keyX = Int()
+        var keyX = BigInt()
         keyX.value = unsafePointerX
         keyX.len = 32
         
-        let YVal : [UInt8] = [0x66, 0x41, 0x8C, 0x4D, 0x7F, 0x25, 0x2C, 0xC2, 0x3E, 0xDF, 0x63, 0xE4, 0x5D, 0xBD, 0x98, 0x45, 0x57, 0xD8, 0xA4, 0xDA, 0x41, 0x2C, 0xC8, 0x12, 0x64, 0x16, 0xE0, 0xA4, 0xF8, 0x1B, 0x33, 0x22];
+        // Y = 10564057289999407626309237453457578977834988122411075958351091519856342060014
+        let YVal : [UInt8] = [0xEE, 0xCB, 0x05, 0xCF, 0xDD, 0x4F, 0x4D, 0x8A, 0x7B, 0xC1, 0x00, 0xD9, 0x60, 0x62, 0xC0, 0xBA, 0x00, 0x0F, 0x98, 0xBC, 0x6D, 0x99, 0xEA, 0xB2, 0x54, 0xEC, 0x46, 0xC3, 0x66, 0x0B, 0x5B, 0x17];
         let unsafePointerY = UnsafeMutablePointer<UInt8>(mutating: YVal)
-        var keyY = Int()
+        var keyY = BigInt()
         keyY.value = unsafePointerY
         keyY.len = 32
         
@@ -62,7 +67,7 @@ public class SwiftPrivadoidPlugin: NSObject, FlutterPlugin {
         for i in 1...8 {
             print("%i:", i)
             for j in 1...32 {
-                print(" %02X", entryRes?.pointee.data[32*i+j])
+                print(String(format:"%02X", entryRes!.pointee.data[32*i+j]))
             }
             print("\n")
         }
@@ -81,7 +86,7 @@ public class SwiftPrivadoidPlugin: NSObject, FlutterPlugin {
         let unsafePointerExpected : UnsafeMutablePointer<UInt8> = UnsafeMutablePointer<UInt8>(mutating: expected)
        
         if (unsafePointerExpected == entryRes?.pointee.data) {
-          IDENFreeTreeEntryResult(entryRes)
+          IDENFreeTreeEntry(entryRes)
           print("unexpected tree entry\n")
           return "ERROR"
         }
@@ -92,62 +97,25 @@ public class SwiftPrivadoidPlugin: NSObject, FlutterPlugin {
          * Test merkle tree
          */
         
-        let mt = IDENnewMerkleTree(40)
-        
+        let mt = createCorrectMT()
         if (mt == nil) {
-            print("unable to allocate merkle tree\n")
-            IDENFreeTreeEntryResult(entryRes)
             return "ERROR"
         }
         
-        if (mt?.pointee.status != IDENTMERKLETREE_OK) {
-            print("error creating merkle tree, code: " + (mt?.pointee.status.rawValue.description)!)
-            if (mt?.pointee.error_msg != nil) {
-                print("error message: " + (mt?.pointee.error_msg.debugDescription)!)
-            }
-            IDENFreeMerkleTree(mt)
-            IDENFreeTreeEntryResult(entryRes)
+        let res = addClaimToMT(mt: mt, entryRes: entryRes)
+        if (res != 0) {
             return "ERROR"
         }
-        
-        print("merkle tree successfuly created\n")
-        
-        let addStatus = IDENmerkleTreeAddClaim(mt, entryRes)
-        
-        // tree entry is not needed anymore
-        IDENFreeTreeEntryResult(entryRes);
-        
-        if (addStatus == nil) {
-            IDENFreeMerkleTree(mt)
-            print("unable to allocate result to add entry to merkle tree")
-            return "ERROR"
-        }
-        
-        if (addStatus?.pointee.status != IDENSTATUSCODE_OK) {
-            print("error add entry to merkle tree, code %i", addStatus?.pointee.status)
-            if (addStatus?.pointee.error_msg != nil) {
-                print(", error message: %s", addStatus?.pointee.error_msg)
-            };
-            print("\n")
-            IDENFreeMerkleTree(mt)
-            IDENFreeStatus(addStatus)
-            return "ERROR"
-        }
-        
-        // addStatus is not needed anymore
-        IDENFreeStatus(addStatus)
-        print("add status successfuly freed\n")
         
         let mtRoot = IDENmerkleTreeRoot(mt)
         if (mtRoot == nil) {
-          IDENFreeMerkleTree(mt)
           print("unable to get merkle tree root\n")
           return "ERROR"
         }
         
         print("Root:")
-        for i in 1...32 {
-            print(" %02X", mtRoot?[i])
+        for i in 0...31 {
+            print(String(format:"%02X", mtRoot![i]))
         }
         print("\n")
         
@@ -159,15 +127,113 @@ public class SwiftPrivadoidPlugin: NSObject, FlutterPlugin {
         
         let unsafePointerExpectedRoot : UnsafeMutablePointer<UInt8> = UnsafeMutablePointer<UInt8>(mutating: expectedRoot)
        
-        if (unsafePointerExpectedRoot == mtRoot) {
+        if (memcmp(unsafePointerExpectedRoot, mtRoot, 32) == 0) {
           print("Generated Tree Root IS CORRECT\n")
+        } else {
+          print("Generated Tree Root IS WRONG\n")
         }
         
-        free(mtRoot)
-        print("tree root successfuly freed\n")
+        let idGenesis = IDENidGenesisFromIdenState(mtRoot)
+        if (idGenesis == nil) {
+            print("unable to get genesis id from iden state\n")
+            return "ERROR"
+        }
         
-        IDENFreeMerkleTree(mt)
-        print("merkle tree successfuly freed\n")
+        let expectedGenesisID : [UInt8] = [
+            0x00, 0x00, 0x47, 0x21, 0xF1, 0x2E, 0xD2, 0xEB,
+            0xEA, 0x79, 0xAB, 0x80, 0x9C, 0xE7, 0x50, 0xB4,
+            0x6A, 0x39, 0xAB, 0x7E, 0x6F, 0xB9, 0x4E, 0xE5,
+            0xE5, 0x25, 0x3B, 0x2B, 0x1F, 0x0E, 0x0F]
+        
+        print("Genesis ID:")
+        for i in 0...30 {
+            print(String(format:"%02X", idGenesis![i]))
+        }
+        print("\n")
+        
+        let unsafePointerExpectedGenesisID : UnsafeMutablePointer<UInt8> = UnsafeMutablePointer<UInt8>(mutating: expectedGenesisID)
+        
+        if ((memcmp(unsafePointerExpectedGenesisID, idGenesis, 31)) == 0) {
+            print("Genesis ID IS CORRECT\n")
+          } else {
+            print("Genesis ID IS WRONG\n")
+          }
+        
+        let indexHash = IDENTreeEntryIndexHash(entryRes)
+        if (indexHash == nil) {
+            print("unable to allocate index hash\n")
+            return "ERROR"
+        }
+        
+        if (indexHash?.pointee.status != IDENHASHSTATUS_OK) {
+            print("cant calc index hash: " + (indexHash?.pointee.status.rawValue.description)!)
+            if (indexHash?.pointee.error_msg != nil) {
+                print("error message: " + (indexHash?.pointee.error_msg.debugDescription)!)
+            }
+            return "ERROR"
+        }
+        
+        let expectedIndexHash: [UInt8] = [
+            0x87, 0xC3, 0x3F, 0xF5, 0xFD, 0x0A, 0xAC, 0x5F,
+                0xF0, 0x25, 0x47, 0x36, 0x07, 0x27, 0xD2, 0x2A,
+                0x2B, 0xB5, 0x26, 0x5A, 0x4B, 0xAB, 0x4A, 0x8C,
+                0xD3, 0xED, 0x13, 0xF1, 0xF8, 0xDB, 0xC1, 0x15]
+        
+        let unsafePointerExpectedIndexHash : UnsafeMutablePointer<UInt8> = UnsafeMutablePointer<UInt8>(mutating: expectedIndexHash)
+        
+        if (memcmp(unsafePointerExpectedIndexHash, indexHash?.pointee.data, (indexHash?.pointee.data_len)!) == 0) {
+            print("Index Hash IS CORRECT\n")
+        } else {
+            print("Index Hash IS WRONG\n")
+        }
+        
+        let proof = IDENmerkleTreeGenerateProof(mt, indexHash)
+        if (proof == nil) {
+            print("unable to allocate proof\n")
+            return "ERROR"
+        }
+        if (proof?.pointee.status != IDENPROOFSTATUS_OK) {
+            print("error generate proof: " + (proof?.pointee.status.rawValue.description)!)
+            if (proof?.pointee.error_msg != nil) {
+                print("error message: " + (proof?.pointee.error_msg.debugDescription)!)
+            }
+            return "ERROR"
+        }
+        
+        print("proof existence: ", proof?.pointee.existence)
+        if (proof?.pointee.existence == false) {
+            return "ERROR"
+        }
+        
+        if (proof != nil) {
+            IDENFreeProof(proof)
+            print("proof successfully freed")
+        }
+        
+        if (indexHash != nil) {
+            IDENFreeHash(indexHash)
+            print("index hash successfully freed")
+        }
+        
+        if (idGenesis != nil) {
+            free(idGenesis)
+            print("id genesis successfully freed")
+        }
+        
+        if (mtRoot != nil) {
+            free(mtRoot)
+            print("tree root successfuly freed\n")
+        }
+    
+        if (mt != nil) {
+            IDENFreeMerkleTree(mt)
+            print("merkle tree successfuly freed\n")
+        }
+            
+        if (entryRes != nil) {
+            IDENFreeTreeEntry(entryRes)
+            print("tree entry successfuly freed")
+        }
         
         return "ALL GOOD";
     }
@@ -194,6 +260,49 @@ public class SwiftPrivadoidPlugin: NSObject, FlutterPlugin {
         }
         
         
+    }
+    
+    public func createCorrectMT() -> UnsafeMutablePointer<IDENmerkleTree>? {
+        let mt = IDENnewMerkleTree(40)
+        
+        if (mt == nil) {
+            print("unable to allocate merkle tree\n")
+            return nil
+        }
+        
+        if (mt?.pointee.status != IDENTMERKLETREE_OK) {
+            print("error creating merkle tree, code: " + (mt?.pointee.status.rawValue.description)!)
+            if (mt?.pointee.error_msg != nil) {
+                print("error message: " + (mt?.pointee.error_msg.debugDescription)!)
+            }
+            IDENFreeMerkleTree(mt)
+            return nil
+        }
+        
+        print("merkle tree successfuly created\n")
+        return mt
+    }
+    
+    public func addClaimToMT(mt: UnsafeMutablePointer<IDENmerkleTree>?, entryRes: UnsafeMutablePointer<IDENTreeEntry>?) -> Int {
+        let addStatus = IDENmerkleTreeAddClaim(mt, entryRes)
+        
+        if (addStatus == nil) {
+            print("unable to allocate result to add entry to merkle tree")
+            return 1
+        }
+        
+        if (addStatus?.pointee.status != IDENSTATUSCODE_OK) {
+            print("error add entry to merkle tree, code %i", addStatus?.pointee.status)
+            if (addStatus?.pointee.error_msg != nil) {
+                print(", error message: %s", addStatus?.pointee.error_msg)
+            };
+            print("\n")
+            IDENFreeStatus(addStatus)
+            return 1
+        }
+        
+        IDENFreeStatus(addStatus)
+        return 0
     }
 
   public func dummyMethodToEnforceBundling() {
