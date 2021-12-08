ec84b90c3bd6527f519eff78b060ba1e332b9e73
Raul Jareño <raul.jareno.diaz@gmail.com>
2021-12-08 14:55:24 +0000

    getting hex array

--- ios/Classes/SwiftPrivadoidPlugin.swift
+++ ios/Classes/SwiftPrivadoidPlugin.swift
@@ -2,6 +2,30 @@ import Flutter
 import UIKit
 import CryptoSwift
 
+enum HexConvertError: Error {
+    case wrongInputStringLength
+    case wrongInputStringCharacters
+}
+
+extension StringProtocol {
+    func asHexArrayFromNonValidatedSource() -> [UInt8] {
+        var startIndex = self.startIndex
+        return stride(from: 0, to: count, by: 2).compactMap { _ in
+            let endIndex = index(startIndex, offsetBy: 2, limitedBy: self.endIndex) ?? self.endIndex
+            defer { startIndex = endIndex }
+            return UInt8(self[startIndex..<endIndex], radix: 16)
+        }
+    }
+
+    func asHexArray() throws -> [UInt8] {
+        if count % 2 != 0 { throw HexConvertError.wrongInputStringLength }
+        let characterSet = "0123456789ABCDEFabcdef"
+        let wrongCharacter = first { return !characterSet.contains($0) }
+        if wrongCharacter != nil { throw HexConvertError.wrongInputStringCharacters }
+        return asHexArrayFromNonValidatedSource()
+    }
+}
+
 public class SwiftPrivadoidPlugin: NSObject, FlutterPlugin {
     
     public static func register(with registrar: FlutterPluginRegistrar) {
@@ -36,8 +60,9 @@ public class SwiftPrivadoidPlugin: NSObject, FlutterPlugin {
         let schemaHash : [UInt8] = [0x52, 0xFD, 0xFC, 0x07, 0x21, 0x82, 0x65, 0x4F, 0x16, 0x3F, 0x5F, 0x0F, 0x9A, 0x62, 0x1D, 0x72]
         let unsafePointerSchemaHash : UnsafeMutablePointer<UInt8> = UnsafeMutablePointer<UInt8>(mutating: schemaHash)
         
-        //let pubXBigInt = "234056d968baf183fe8d237d496d1c04188220cd33e8f8d14df9b84479736b20"
-        let XVal : [UInt8] = [0x23, 0x40, 0x56, 0xd9, 0x68, 0xba, 0xf1, 0x83, 0xfe, 0x8d, 0x23, 0x7d, 0x49, 0x6d, 0x1c, 0x04, 0x18, 0x82, 0x20, 0xcd, 0x33, 0xe8, 0xf8, 0xd1, 0x4d, 0xf9, 0xb8, 0x44, 0x79, 0x73, 0x6b, 0x20]
+        let pubXBigInt = "152f5044240ef872cf7e6742fe202b9e07ed6188e9e734c09b06939704852358"
+        /*let XVal : [UInt8] = [0x23, 0x40, 0x56, 0xd9, 0x68, 0xba, 0xf1, 0x83, 0xfe, 0x8d, 0x23, 0x7d, 0x49, 0x6d, 0x1c, 0x04, 0x18, 0x82, 0x20, 0xcd, 0x33, 0xe8, 0xf8, 0xd1, 0x4d, 0xf9, 0xb8, 0x44, 0x79, 0x73, 0x6b, 0x20]*/
+        let XVal = pubXBigInt.asHexArrayFromNonValidatedSource()
         //let XVal : [UInt8] = Array<UInt8>.init(hex: pubXBigInt)
         let unsafePointerX : UnsafeMutablePointer<UInt8> = UnsafeMutablePointer<UInt8>(mutating: XVal)
         var keyX = BigInt()
@@ -45,14 +70,14 @@ public class SwiftPrivadoidPlugin: NSObject, FlutterPlugin {
         keyX.len = 32
         print(XVal)
         
-        //let pubYBigInt = "2624393fad9b71c04b3b14d8ac45202dbb4eaff4c2d1350c9453fc08d18651fe"
-        let YVal : [UInt8] = [0x26, 0x24, 0x39, 0x3f, 0xad, 0x9b, 0x71, 0xc0, 0x4b, 0x3b, 0x14, 0xd8, 0xac, 0x45, 0x20, 0x2d, 0xbb, 0x4e, 0xaf, 0xf4, 0xc2, 0xd1, 0x35, 0x0c, 0x94, 0x53, 0xfc, 0x08, 0xd1, 0x86, 0x51, 0xfe]
+        let pubYBigInt = "2865441cd3e276643c84e55004ad259dff282c8c47c6e8c151afacdadf6f6db3"
+        //let YVal : [UInt8] = [0x26, 0x24, 0x39, 0x3f, 0xad, 0x9b, 0x71, 0xc0, 0x4b, 0x3b, 0x14, 0xd8, 0xac, 0x45, 0x20, 0x2d, 0xbb, 0x4e, 0xaf, 0xf4, 0xc2, 0xd1, 0x35, 0x0c, 0x94, 0x53, 0xfc, 0x08, 0xd1, 0x86, 0x51, 0xfe]
+        let YVal = pubYBigInt.asHexArrayFromNonValidatedSource()
         //let YVal : [UInt8] = Array<UInt8>.init(hex: pubYBigInt)
         let unsafePointerY = UnsafeMutablePointer<UInt8>(mutating: YVal)
         var keyY = BigInt()
         keyY.value = unsafePointerY
         keyY.len = 32
-        
         print(YVal)
         
         let revNonce = UInt64(13260572831089785859)
@@ -67,7 +92,7 @@ public class SwiftPrivadoidPlugin: NSObject, FlutterPlugin {
         if (entryRes?.pointee.status != IDENTREEENTRY_OK) {
             print("error creating tree entry\n")
             if (entryRes?.pointee.error_msg != nil) {
-                print("error message: " + (entryRes?.pointee.error_msg.pointee.description)!)
+                print((entryRes?.pointee.error_msg)!)
             }
             return "ERROR"
         }
--- lib/privadoid_sdk.dart
+++ lib/privadoid_sdk.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:typed_data';
 
 import 'package:flutter/services.dart';
 import 'package:privadoid_sdk/privadoid_wallet.dart';
@@ -13,9 +14,10 @@ class PrivadoIdSdk {
   }
 
   static Future<String?> createNewIdentity() async {
-    final PrivadoIdWallet wallet =
-        await PrivadoIdWallet.createPrivadoIdWallet(null);
-    HexUtils.hexToBytes(wallet.publicKeyHex[0]);
+    final PrivadoIdWallet wallet = await PrivadoIdWallet.createPrivadoIdWallet(
+        HexUtils.hexToBytes(
+            "21a5e7321d0e2f3ca1cc6504396e6594a2211544b08c206847cdee96f832421a"));
+    Uint8List bytes = HexUtils.hexToBytes(wallet.publicKeyHex[0]);
     final String? version = await _channel.invokeMethod(
         'createNewIdentity', [wallet.publicKeyHex[0], wallet.publicKeyHex[1]]);
     return version;
