9eda3f581085c4bfad20c3b52ae51780e2d68ffb
Raul Jareño <raul.jareno.diaz@gmail.com>
2022-06-07 13:23:13 +0200

    added methods for witness sig and mtp

--- lib/libs/iden3corelib.dart
+++ lib/libs/iden3corelib.dart
@@ -1830,6 +1830,126 @@ class Iden3CoreLib {
     return null;
   }
 
+  Future<Uint8List?> calculateWitnessSig(
+      Uint8List wasmBytes, Uint8List inputsJsonBytes) async {
+    int circuitSize = wasmBytes.length;
+    ffi.Pointer<ffi.Char> circuitBuffer = malloc<ffi.Char>(circuitSize);
+    final data = wasmBytes;
+    for (int i = 0; i < circuitSize; i++) {
+      circuitBuffer[i] = data[i];
+    }
+
+    int jsonSize = inputsJsonBytes.length;
+    ffi.Pointer<ffi.Char> jsonBuffer = malloc<ffi.Char>(jsonSize);
+    final data2 = inputsJsonBytes;
+    for (int i = 0; i < jsonSize; i++) {
+      jsonBuffer[i] = data2[i];
+    }
+
+    ffi.Pointer<ffi.UnsignedLong> wtnsSize = malloc<ffi.UnsignedLong>();
+    wtnsSize.value = 4 * 1024 * 1024;
+    ffi.Pointer<ffi.Char> wtnsBuffer = malloc<ffi.Char>(wtnsSize.value);
+
+    int errorMaxSize = 256;
+    ffi.Pointer<ffi.Char> errorMsg = malloc<ffi.Char>(errorMaxSize);
+
+    int result = _witnessSigLib.witnesscalc_credentialAtomicQuerySig(
+        circuitBuffer,
+        circuitSize,
+        jsonBuffer,
+        jsonSize,
+        wtnsBuffer,
+        wtnsSize,
+        errorMsg,
+        errorMaxSize);
+    if (result == witness.WITNESSCALC_OK) {
+      Uint8List wtnsBytes = Uint8List(wtnsSize.value);
+      for (int i = 0; i < wtnsSize.value; i++) {
+        wtnsBytes[i] = wtnsBuffer[i];
+      }
+      /*ffi.Pointer<Utf8> jsonString = wtnsBuffer.cast<Utf8>();
+      String wtnsmsg = jsonString.toDartString();
+      if (kDebugMode) {
+        print("$result: ${result.toString()}");
+        print("Wtns: $wtnsmsg");
+      }*/
+      return wtnsBytes;
+    } else if (result == witness.WITNESSCALC_ERROR) {
+      //ffi.Pointer<ffi.Int8> json = errorMsg.cast<ffi.Int8>();
+      ffi.Pointer<Utf8> jsonString = errorMsg.cast<Utf8>();
+      String errormsg = jsonString.toDartString();
+      if (kDebugMode) {
+        print("$result: ${result.toString()}. Error: $errormsg");
+      }
+    } else if (result == witness.WITNESSCALC_ERROR_SHORT_BUFFER) {
+      if (kDebugMode) {
+        print(
+            "$result: ${result.toString()}. Error: Short buffer for proof or public");
+      }
+    }
+    return null;
+  }
+
+  Future<Uint8List?> calculateWitnessMtp(
+      Uint8List wasmBytes, Uint8List inputsJsonBytes) async {
+    int circuitSize = wasmBytes.length;
+    ffi.Pointer<ffi.Char> circuitBuffer = malloc<ffi.Char>(circuitSize);
+    final data = wasmBytes;
+    for (int i = 0; i < circuitSize; i++) {
+      circuitBuffer[i] = data[i];
+    }
+
+    int jsonSize = inputsJsonBytes.length;
+    ffi.Pointer<ffi.Char> jsonBuffer = malloc<ffi.Char>(jsonSize);
+    final data2 = inputsJsonBytes;
+    for (int i = 0; i < jsonSize; i++) {
+      jsonBuffer[i] = data2[i];
+    }
+
+    ffi.Pointer<ffi.UnsignedLong> wtnsSize = malloc<ffi.UnsignedLong>();
+    wtnsSize.value = 4 * 1024 * 1024;
+    ffi.Pointer<ffi.Char> wtnsBuffer = malloc<ffi.Char>(wtnsSize.value);
+
+    int errorMaxSize = 256;
+    ffi.Pointer<ffi.Char> errorMsg = malloc<ffi.Char>(errorMaxSize);
+
+    int result = _witnessMtpLib.witnesscalc_credentialAtomicQueryMTP(
+        circuitBuffer,
+        circuitSize,
+        jsonBuffer,
+        jsonSize,
+        wtnsBuffer,
+        wtnsSize,
+        errorMsg,
+        errorMaxSize);
+    if (result == witness.WITNESSCALC_OK) {
+      Uint8List wtnsBytes = Uint8List(wtnsSize.value);
+      for (int i = 0; i < wtnsSize.value; i++) {
+        wtnsBytes[i] = wtnsBuffer[i];
+      }
+      /*ffi.Pointer<Utf8> jsonString = wtnsBuffer.cast<Utf8>();
+      String wtnsmsg = jsonString.toDartString();
+      if (kDebugMode) {
+        print("$result: ${result.toString()}");
+        print("Wtns: $wtnsmsg");
+      }*/
+      return wtnsBytes;
+    } else if (result == witness.WITNESSCALC_ERROR) {
+      //ffi.Pointer<ffi.Int8> json = errorMsg.cast<ffi.Int8>();
+      ffi.Pointer<Utf8> jsonString = errorMsg.cast<Utf8>();
+      String errormsg = jsonString.toDartString();
+      if (kDebugMode) {
+        print("$result: ${result.toString()}. Error: $errormsg");
+      }
+    } else if (result == witness.WITNESSCALC_ERROR_SHORT_BUFFER) {
+      if (kDebugMode) {
+        print(
+            "$result: ${result.toString()}. Error: Short buffer for proof or public");
+      }
+    }
+    return null;
+  }
+
   Future<Map<String, dynamic>?> prove(
       Uint8List zkeyBytes, Uint8List wtnsBytes) async {
     Map<String, dynamic> map = {};
