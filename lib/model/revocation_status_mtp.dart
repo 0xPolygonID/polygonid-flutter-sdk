0270c4121f8ed8ec97c4105e2366ad4dce58e7c6
Dmytro Sukhyi <dmytro.sukhiy@gmail.com>
2022-07-07 13:09:22 +0300

    [WIP]update revocation status

--- lib/model/revocation_status_mtp.dart
+++ lib/model/revocation_status_mtp.dart
@@ -1,27 +1,46 @@
+import 'dart:convert';
+
+import 'package:json_annotation/json_annotation.dart';
+import 'package:privadoid_sdk/model/mtp_node_aux.dart';
+
+@JsonSerializable()
 class RevocationStatusMtp {
+  @JsonKey(ignore: true)
   final String? type;
   final bool? existence;
   final List<dynamic>? siblings;
+  final NodeAux? nodeAux;
+
 
   // TODO: add NodeAux
 
-  RevocationStatusMtp({this.type, this.existence, this.siblings});
+  RevocationStatusMtp({this.type, this.existence, this.siblings, this.nodeAux});
 
   /// Creates an instance from the given json
   ///
   /// @param [Map<String, dynamic>] json
   /// @returns [RevocationStatusMtp]
   factory RevocationStatusMtp.fromJson(Map<String, dynamic> json) {
+    NodeAux? nodeAux;
+    try {
+      nodeAux = NodeAux.fromJson(json['node_aux']);
+    } catch (e) {
+      nodeAux = null;
+    }
+
     return RevocationStatusMtp(
       type: json['@type'],
       existence: json['existence'],
       siblings: json['siblings'],
+      nodeAux: nodeAux,
     );
   }
 
-  Map<String, String> toJson() => {
-        '@type': type.toString(),
-        'userReference': existence.toString(),
-        'siblings': siblings.toString(),
-      };
+  Map<String, dynamic> toJson() => {
+        '@type': type?.toString(),
+        'existence': existence,
+        'siblings': siblings,
+        'node_aux': nodeAux?.toJson(),
+      }..removeWhere(
+          (dynamic key, dynamic value) => key == null || value == null);
 }
