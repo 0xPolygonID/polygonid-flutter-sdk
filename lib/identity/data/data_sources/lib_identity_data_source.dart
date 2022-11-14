5ff60f0258716f30fe8497d76acb0f7c3409a692
Raul Jareño <raul.jareno.diaz@gmail.com>
2022-11-14 10:04:20 +0100

    removing commented code

--- lib/identity/data/data_sources/lib_identity_data_source.dart
+++ lib/identity/data/data_sources/lib_identity_data_source.dart
@@ -1,21 +1,15 @@
 import 'package:fast_base58/fast_base58.dart';
 import 'package:flutter/foundation.dart';
-import 'package:polygonid_flutter_sdk/identity/data/repositories/smt_memory_storage_repository_impl.dart';
-import 'package:polygonid_flutter_sdk/identity/libs/smt/merkletree.dart';
 import 'package:polygonid_flutter_sdk/proof_generation/domain/exceptions/proof_generation_exceptions.dart';
 
 import '../../../common/utils/hex_utils.dart';
-import '../../domain/repositories/smt_storage_repository.dart';
 import '../../libs/iden3core/iden3core.dart';
 
 class LibIdentityDataSource {
   final Iden3CoreLib _iden3coreLib;
 
-  //final SMTStorageRepository _smtStorageRepository;
-
   LibIdentityDataSource(
     this._iden3coreLib,
-    /*this._smtStorageRepository*/
   );
 
   Future<String> getId(String id) {
