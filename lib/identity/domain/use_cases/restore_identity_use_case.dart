9799f28b7c22458523d10d22e7734e5cd5b3fe96
Raul Jareño <raul.jareno.diaz@gmail.com>
2023-02-10 13:50:46 +0100

    fixing restore identity

--- lib/identity/domain/use_cases/restore_identity_use_case.dart
+++ lib/identity/domain/use_cases/restore_identity_use_case.dart
@@ -1,7 +1,10 @@
+import 'package:polygonid_flutter_sdk/identity/domain/use_cases/get_identity_use_case.dart';
+
 import '../../../common/domain/domain_logger.dart';
 import '../../../common/domain/use_case.dart';
 import '../entities/identity_entity.dart';
 import '../entities/private_identity_entity.dart';
+import '../exceptions/identity_exceptions.dart';
 import 'create_and_save_identity_use_case.dart';
 import 'get_did_identifier_use_case.dart';
 import 'import_identity_use_case.dart';
@@ -23,11 +26,13 @@ class RestoreIdentityParam {
 class RestoreIdentityUseCase
     extends FutureUseCase<RestoreIdentityParam, IdentityEntity> {
   final CreateAndSaveIdentityUseCase _createAndSaveIdentityUseCase;
+  final GetIdentityUseCase _getIdentityUseCase;
   final ImportIdentityUseCase _importIdentityUseCase;
   final GetDidIdentifierUseCase _getDidIdentifierUseCase;
 
   RestoreIdentityUseCase(
     this._createAndSaveIdentityUseCase,
+    this._getIdentityUseCase,
     this._importIdentityUseCase,
     this._getDidIdentifierUseCase,
   );
@@ -35,27 +40,43 @@ class RestoreIdentityUseCase
   @override
   Future<PrivateIdentityEntity> execute(
       {required RestoreIdentityParam param}) async {
-    try {
-      // Create the [PrivateIdentityEntity] with the secret
-      PrivateIdentityEntity privateIdentity =
-          await _createAndSaveIdentityUseCase.execute(
-              param: CreateAndSaveIdentityParam(
-                  privateKey: param.privateKey,
-                  blockchain: param.blockchain,
-                  network: param.network));
 
-      if (param.encryptedIdentityDbs != null) {
+    PrivateIdentityEntity? privateIdentity;
+    try {
+      String genesisDid = await _getDidIdentifierUseCase.execute(
+          param: GetDidIdentifierParam(
+              privateKey: param.privateKey,
+              blockchain: param.blockchain,
+              network: param.network));
+      privateIdentity = await _getIdentityUseCase.execute(param: GetIdentityParam(did: genesisDid, privateKey: param.privateKey)) as PrivateIdentityEntity;
+    } on UnknownIdentityException {
+
+      privateIdentity =
+      await _createAndSaveIdentityUseCase.execute(
+          param: CreateAndSaveIdentityParam(
+              privateKey: param.privateKey,
+              blockchain: param.blockchain,
+              network: param.network));
+
+    } catch (error) {
+      logger().e("[RestoreIdentityUseCase] Error: $error");
+
+      rethrow;
+    }
+
+    try {
+      if (param.encryptedIdentityDbs != null && privateIdentity != null) {
         for (MapEntry<int, String> identityDb
             in param.encryptedIdentityDbs!.entries) {
           String profileDid = await _getDidIdentifierUseCase.execute(
               param: GetDidIdentifierParam(
-                  privateKey: privateIdentity.privateKey,
+                  privateKey: param.privateKey,
                   blockchain: param.blockchain,
                   network: param.network,
                   profileNonce: identityDb.key));
           await _importIdentityUseCase.execute(
               param: ImportIdentityParam(
-            privateKey: privateIdentity.privateKey,
+            privateKey: param.privateKey,
             did: profileDid,
             encryptedDb: identityDb.value,
           ));
