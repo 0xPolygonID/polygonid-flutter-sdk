af9a10ed6742dc87c7c8f90b2ff758072d6a8fc8
Raul J <raul.jareno.diaz@gmail.com>
2023-05-17 03:35:09 -0700

    fixed iden3comm_proofs_use_case

--- example/lib/src/presentation/app.dart
+++ example/lib/src/presentation/app.dart
@@ -16,7 +16,6 @@ class AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      useInheritedMediaQuery: true,
       title: CustomStrings.appTitle,
       initialRoute: Routes.initialPath,
       routes: Routes.getRoutes(context),
