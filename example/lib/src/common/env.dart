c4bca387a3d3764a41e5ccce7e6ce9420a2daded
Raul J <raul.jareno.diaz@gmail.com>
2023-05-22 03:27:02 -0700

    fixed envied

--- example/lib/src/common/env.dart
+++ example/lib/src/common/env.dart
@@ -2,7 +2,7 @@ import 'package:envied/envied.dart';
 
 part 'env.g.dart';
 
-@Envied(name: 'Env', path: 'env')
+@Envied()
 abstract class Env {
   @EnviedField(varName: 'ENV_POLYGON_MUMBAI')
   static const String polygonMumbai = _Env.polygonMumbai;
