305ae5bcdcba0b55a26bc5cdfde9257b3712dc4d
Dmytro Sukhyi <dmytro.sukhiy@gmail.com>
2023-06-19 17:20:50 +0100

    add IPFS RPC to c-lib configuration

--- lib/proof/data/dtos/atomic_query_inputs_config_param.dart
+++ lib/proof/data/dtos/atomic_query_inputs_config_param.dart
@@ -5,15 +5,18 @@ import 'gist_proof_dto.dart';
 class AtomicQueryInputsConfigParam {
   final String ethereumUrl;
   final String stateContractAddr;
+  final String ipfsNodeURL;
 
   AtomicQueryInputsConfigParam({
     required this.ethereumUrl,
     required this.stateContractAddr,
+    required this.ipfsNodeURL,
   });
 
   Map<String, dynamic> toJson() => {
         "ethereumUrl": ethereumUrl,
         "stateContractAddr": stateContractAddr,
+        "ipfsNodeURL": ipfsNodeURL,
       }..removeWhere(
           (dynamic key, dynamic value) => key == null || value == null);
 
@@ -21,6 +24,7 @@ class AtomicQueryInputsConfigParam {
     return AtomicQueryInputsConfigParam(
       ethereumUrl: json['ethereumUrl'],
       stateContractAddr: json['stateContractAddr'],
+        ipfsNodeURL: json['ipfsNodeURL'],
     );
   }
 }
