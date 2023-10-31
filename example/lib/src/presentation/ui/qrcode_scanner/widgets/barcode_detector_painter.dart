f76e5f53e533d68c359a592acfa2e2537365aab8
emuroni <emuroni@polygon.technology>
2023-10-31 15:23:54 +0100

    example app qr code scanner

--- example/lib/src/presentation/ui/qrcode_scanner/widgets/barcode_detector_painter.dart
+++ example/lib/src/presentation/ui/qrcode_scanner/widgets/barcode_detector_painter.dart
@@ -1,4 +1,3 @@
-/*
 import 'dart:math';
 import 'dart:ui';
 import 'dart:ui' as ui;
@@ -97,4 +96,3 @@ class BarcodeDetectorPainter extends CustomPainter {
         oldDelegate.barcodes != barcodes;
   }
 }
-*/
