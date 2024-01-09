190f10ab2c2c528e9fbd3e91d27d36e8ad61f9d0
Yaroslav Moria <5eeman@users.noreply.github.com>
2024-01-09 17:53:09 +0100

    Rebuilt injector

--- lib/sdk/di/injector.config.dart
+++ lib/sdk/di/injector.config.dart
@@ -436,7 +436,6 @@ extension GetItInjectableX on _i1.GetIt {
     gh.factory<_i10.ClaimStateMapper>(() => _i10.ClaimStateMapper());
     gh.factory<_i11.Client>(() => networkModule.client);
     gh.factory<_i12.CreatePathWrapper>(() => _i12.CreatePathWrapper());
-    gh.lazySingletonAsync<_i13.Database>(() => databaseModule.database());
     gh.factoryParamAsync<_i13.Database, String?, String?>(
       (
         identifier,
@@ -448,6 +447,7 @@ extension GetItInjectableX on _i1.GetIt {
       ),
       instanceName: 'polygonIdSdkIdentity',
     );
+    gh.lazySingletonAsync<_i13.Database>(() => databaseModule.database());
     gh.factory<_i12.DestinationPathDataSource>(
         () => _i12.DestinationPathDataSource(gh<_i12.CreatePathWrapper>()));
     gh.factory<_i14.DidProfileInfoInteractedDidFilterMapper>(
@@ -543,20 +543,12 @@ extension GetItInjectableX on _i1.GetIt {
     gh.lazySingleton<_i64.StacktraceManager>(() => _i64.StacktraceManager());
     gh.factory<_i65.StateIdentifierMapper>(() => _i65.StateIdentifierMapper());
     gh.factory<_i13.StoreRef<String, Map<String, Object?>>>(
-      () => databaseModule.profileStore,
-      instanceName: 'profilesStore',
-    );
-    gh.factory<_i13.StoreRef<String, Map<String, Object?>>>(
       () => databaseModule.identityStore,
       instanceName: 'identityStore',
     );
     gh.factory<_i13.StoreRef<String, dynamic>>(
       () => databaseModule.keyValueStore,
       instanceName: 'keyValueStore',
-    );
-    gh.factory<_i13.StoreRef<String, Map<String, Object?>>>(
-      () => databaseModule.interactionStore,
-      instanceName: 'interactionStore',
     );
     gh.factory<_i13.StoreRef<String, Map<String, Object?>>>(
       () => databaseModule.claimStore,
@@ -569,6 +561,10 @@ extension GetItInjectableX on _i1.GetIt {
     gh.factory<_i13.StoreRef<String, Map<String, Object?>>>(
       () => databaseModule.didProfileInfoStore,
       instanceName: 'didProfileInfoStore',
+    );
+    gh.factory<_i13.StoreRef<String, Map<String, Object?>>>(
+      () => databaseModule.profileStore,
+      instanceName: 'profilesStore',
     );
     gh.factory<_i66.TreeStateMapper>(() => _i66.TreeStateMapper());
     gh.factory<_i67.TreeTypeMapper>(() => _i67.TreeTypeMapper());
@@ -772,10 +768,11 @@ extension GetItInjectableX on _i1.GetIt {
               gh<_i22.FiltersMapper>(),
               gh<_i33.InteractionIdFilterMapper>(),
             ));
-    gh.factory<_i98.LibPolygonIdCoreProofDataSource>(() =>
-        _i98.LibPolygonIdCoreProofDataSource(
-            gh<_i98.LibPolygonIdCoreWrapper>(),
-          gh<_i64.StacktraceManager>(),));
+    gh.factory<_i98.LibPolygonIdCoreProofDataSource>(
+        () => _i98.LibPolygonIdCoreProofDataSource(
+              gh<_i98.LibPolygonIdCoreWrapper>(),
+              gh<_i64.StacktraceManager>(),
+            ));
     gh.factory<_i118.SMTDataSource>(() => _i118.SMTDataSource(
           gh<_i27.HexMapper>(),
           gh<_i36.LibBabyJubJubDataSource>(),
