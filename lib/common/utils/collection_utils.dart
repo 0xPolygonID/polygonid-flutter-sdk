d758deb747ecc4701603c44610057f2f76833258
Yaroslav Moria <5eeman@users.noreply.github.com>
2024-05-20 15:37:02 +0200

    Added status field to credential offer data.

---
+++ lib/common/utils/collection_utils.dart
@@ -0,0 +1,10 @@
+extension ListUtils<T> on List<T> {
+  T? firstWhereOrNull(bool Function(T) test) {
+    for (final element in this) {
+      if (test(element)) {
+        return element;
+      }
+    }
+    return null;
+  }
+}
