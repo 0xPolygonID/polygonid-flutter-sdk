cacdc5d32f0e491d1c6b46276a5476c76b5e6a66
emuroni <emuroni@privado.id>
2024-09-25 12:40:03 +0200

    updated clib that includes support of zkEVM

--- android/src/main/jniLibs/arm64-v8a/libpolygonid.h
+++ android/src/main/jniLibs/arm64-v8a/libpolygonid.h
@@ -99,6 +99,7 @@ extern GoUint8 PLGNAuthV2InputsMarshal(char** jsonResponse, char* in, PLGNStatus
 //
 extern GoUint8 PLGNCalculateGenesisID(char** jsonResponse, char* in, PLGNStatus** status);
 extern GoUint8 PLGNNewGenesisID(char** jsonResponse, char* in, char* cfg, PLGNStatus** status);
+extern GoUint8 PLGNNewGenesisIDFromEth(char** jsonResponse, char* in, char* cfg, PLGNStatus** status);
 extern GoUint8 PLGNCreateClaim(char** jsonResponse, char* in, PLGNStatus** status);
 
 // PLGNIDToInt returns the ID as a big int string
