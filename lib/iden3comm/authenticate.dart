378bee020c7b6f7ee1bfabf46eb1223f2bf0ef1e
Yaroslav Moria <5eeman@users.noreply.github.com>
2025-04-03 19:05:08 +0200

    Updated Dio cache interceptor.

--- lib/iden3comm/authenticate.dart
+++ lib/iden3comm/authenticate.dart
@@ -8,7 +8,7 @@ import 'package:http/http.dart' as http;
 
 import 'package:dio/dio.dart';
 import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
-import 'package:dio_cache_interceptor_hive_store/dio_cache_interceptor_hive_store.dart';
+import 'package:http_cache_hive_store/http_cache_hive_store.dart';
 import 'package:encrypt/encrypt.dart';
 import 'package:flutter/foundation.dart';
 import 'package:intl/intl.dart';
@@ -691,7 +691,6 @@ class Authenticate {
         options: CacheOptions(
           store: HiveCacheStore(path),
           policy: CachePolicy.request,
-          hitCacheOnErrorExcept: [],
           maxStale: const Duration(days: 14),
           priority: CachePriority.high,
         ),
@@ -929,7 +928,6 @@ class Authenticate {
         options: CacheOptions(
           store: HiveCacheStore(path),
           policy: CachePolicy.request,
-          hitCacheOnErrorExcept: [],
           maxStale: const Duration(days: 7),
           priority: CachePriority.high,
         ),
