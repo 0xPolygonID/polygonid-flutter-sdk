2167f675a65315d2efa4b2877933124277316038
Yaroslav Moria <5eeman@users.noreply.github.com>
2025-05-26 19:04:56 +0200

    Added docs and util functions.

--- lib/sdk/util.dart
+++ lib/sdk/util.dart
@@ -7,6 +7,10 @@ class Util {
 
   Util(this._polygonIdCoreUtil);
 
+  /// Validates the attestation document and returns the public key and its components.
+  /// [attestationDocument] - base64 encoded attestation document.
+  /// Returns an [AttestationResult] containing the public key and its components.
+  /// If the attestation document is invalid, it will throw an [CoreLibraryException].
   AttestationResult validateAttestationDocument(String attestationDocument) {
     final result =
         _polygonIdCoreUtil.validateAttestationDocument(attestationDocument);
@@ -23,7 +27,11 @@ class Util {
 }
 
 class AttestationResult {
+  /// The public key base64 encoded PKIX, ASN.1 DER form.
   final String publicKey;
+
+  /// X & Y only would be presented if EC signing was used.
+  /// If RSA used, there would be only publicKey.
   final String? publicKeyX;
   final String? publicKeyY;
 
@@ -32,4 +40,22 @@ class AttestationResult {
     this.publicKeyX,
     this.publicKeyY,
   });
+
+  @override
+  String toString() {
+    return 'AttestationResult{publicKey: $publicKey, publicKeyX: $publicKeyX, publicKeyY: $publicKeyY}';
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is AttestationResult &&
+          runtimeType == other.runtimeType &&
+          publicKey == other.publicKey &&
+          publicKeyX == other.publicKeyX &&
+          publicKeyY == other.publicKeyY;
+
+  @override
+  int get hashCode =>
+      publicKey.hashCode ^ publicKeyX.hashCode ^ publicKeyY.hashCode;
 }
