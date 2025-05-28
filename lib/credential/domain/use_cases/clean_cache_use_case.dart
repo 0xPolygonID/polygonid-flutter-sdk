3479e79a18a41df0026f91bd367b7bc2af089dfa
Yaroslav Moria <5eeman@users.noreply.github.com>
2025-05-28 17:37:53 +0200

    Added credential cache force clean on cache clean error.

--- lib/credential/domain/use_cases/clean_cache_use_case.dart
+++ lib/credential/domain/use_cases/clean_cache_use_case.dart
@@ -1,8 +1,13 @@
 import 'dart:convert';
+import 'dart:io';
 
+import 'package:path_provider/path_provider.dart';
+import 'package:polygonid_flutter_sdk/common/domain/domain_logger.dart';
 import 'package:polygonid_flutter_sdk/common/domain/entities/env_config_entity.dart';
+import 'package:polygonid_flutter_sdk/common/domain/error_exception.dart';
 import 'package:polygonid_flutter_sdk/common/domain/use_case.dart';
 import 'package:polygonid_flutter_sdk/common/domain/use_cases/get_env_use_case.dart';
+import 'package:polygonid_flutter_sdk/common/utils/collection_utils.dart';
 import 'package:polygonid_flutter_sdk/credential/domain/repositories/credential_repository.dart';
 
 class CleanCredentialCacheUseCase
@@ -19,16 +24,58 @@ class CleanCredentialCacheUseCase
   Future<void> execute({
     required EnvConfigEntity? param,
   }) async {
-    String? config;
+    final EnvConfigEntity configEntity;
     if (param != null) {
-      config = jsonEncode(param.toJson());
+      configEntity = param;
     } else {
       final env = await _getEnvUseCase.execute();
-      config = jsonEncode(env.config.toJson());
+      configEntity = env.config;
     }
 
-    return _credentialRepository.cleanCache(
-      config: config,
-    );
+    final config = jsonEncode(configEntity.toJson());
+
+    try {
+      _credentialRepository.cleanCache(
+        config: config,
+      );
+    } on CoreLibraryException catch (e) {
+      await _tryEraseCache(configEntity);
+    }
+
+    return;
+  }
+}
+
+Future<void> _tryEraseCache(EnvConfigEntity config) async {
+  final configCachePath = config.cacheDir;
+  final cacheDir = configCachePath == null ? null : Directory(configCachePath);
+  final possibleLocations = {
+    if (cacheDir != null) cacheDir,
+    await getApplicationCacheDirectory(),
+    await getApplicationDocumentsDirectory(),
+  };
+
+  for (final location in possibleLocations) {
+    _tryDeleteCPolygonIDCache(location);
+  }
+}
+
+const _cacheFolderName = "c-polygonid-cache";
+
+void _tryDeleteCPolygonIDCache(Directory location) {
+  if (!location.existsSync()) {
+    return;
+  }
+
+  final cPolygonIdCacheDir = location
+      .listSync()
+      .firstWhereOrNull((f) => f.path.contains(_cacheFolderName));
+  if (cPolygonIdCacheDir != null && cPolygonIdCacheDir.existsSync()) {
+    try {
+      cPolygonIdCacheDir.deleteSync(recursive: true);
+    } catch (e) {
+      // Log the error or handle it as needed
+      logger().d("Failed to delete cache directory: ${e.toString()}");
+    }
   }
 }
