8732d1c2a1555d3c31b2dfaad3428036075f3b8e
demonshd <dmytro.sukhyi@proton.me>
2025-10-06 11:51:07 +0100

    make id and thid optional with generated UUID if not passed

--- lib/iden3comm/domain/entities/resource_management/permissions_list_fetch.dart
+++ lib/iden3comm/domain/entities/resource_management/permissions_list_fetch.dart
@@ -1,20 +1,24 @@
 import 'package:polygonid_flutter_sdk/iden3comm/domain/entities/common/attachment.dart';
 import 'package:polygonid_flutter_sdk/iden3comm/domain/entities/common/iden3_message_entity.dart';
+import 'package:uuid/uuid.dart';
 
 /// Message sent to request the list of permission request sent for user`s resources.
 /// The response should be a [ResourcePermissionsListMessage].
 class ResourcePermissionsListFetchMessage extends Iden3Message<EmptyBody> {
   ResourcePermissionsListFetchMessage({
-    required super.id,
+    String? id,
     super.typ,
-    super.thid,
+    String? thid,
     super.body = const EmptyBody(),
     required super.from,
     super.to,
     super.createdTime,
     super.expiresTime,
     super.attachments = const [],
-  }) : super(type: Iden3MessageType.permissionsListFetch);
+  }) : super(
+          id: id ?? Uuid().v4(),
+          thid: thid ?? Uuid().v4(),
+      type: Iden3MessageType.permissionsListFetch);
 
   factory ResourcePermissionsListFetchMessage.fromJson(
       Map<String, dynamic> json) {
--- lib/iden3comm/domain/entities/resource_management/permissions_requests_list.dart
+++ lib/iden3comm/domain/entities/resource_management/permissions_requests_list.dart
@@ -1,20 +1,24 @@
 import 'package:polygonid_flutter_sdk/iden3comm/domain/entities/common/attachment.dart';
 import 'package:polygonid_flutter_sdk/iden3comm/domain/entities/common/iden3_message_entity.dart';
+import 'package:uuid/uuid.dart';
 
 /// Message sent to agent to track status of requests user sent for resource access.
 /// The response should be a [ResourcePermissionsListMessage].
 class ResourcePermissionsRequestsListMessage extends Iden3Message<EmptyBody> {
   ResourcePermissionsRequestsListMessage({
-    required super.id,
+    String? id,
     super.typ,
-    super.thid,
+    String? thid,
     super.body = const EmptyBody(),
     required super.from,
     super.to,
     super.createdTime,
     super.expiresTime,
     super.attachments = const [],
-  }) : super(type: Iden3MessageType.permissionsRequestsList);
+  }) : super(
+          id: id ?? Uuid().v4(),
+          thid: thid ?? Uuid().v4(),
+          type: Iden3MessageType.permissionsRequestsList);
 
   factory ResourcePermissionsRequestsListMessage.fromJson(
       Map<String, dynamic> json) {
